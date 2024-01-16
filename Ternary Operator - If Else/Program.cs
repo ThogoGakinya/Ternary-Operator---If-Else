382a63b8562982caf1dc2399eaabaf5cffcdade3
ThogoGakinya <thogosamson@gmail.com>
2024-01-16 19:56:52 +0300

    Add project files.

---
+++ Ternary Operator - If Else/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ternary_Operator___If_Else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Checker();
+        }
+
+        public static void Checker() 
+        {
+            int storedTemp;
+            Console.WriteLine("Please Enter Temperature");
+            string temp = Console.ReadLine();
+
+            bool checkInteger = int.TryParse(temp, out storedTemp);
+
+            if (checkInteger) 
+            {
+                string status = storedTemp <= 15 ? "It is too cold here" : storedTemp >= 16 && storedTemp <= 28 ? "It is Ok" : "It is not here";
+                Console.WriteLine(status);
+            }
+            else
+            {
+                Console.WriteLine("Please enter an iunteger");
+            }
+
+            
+        }
+    }
+}
